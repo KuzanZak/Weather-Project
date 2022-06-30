3eb475e411f9b9f034c6ba5654a6feffab9bd3cb
Musito <Mrmusito@gmail.com>
2022-06-30 13:50:35 +0200

    interface js + css + id html

--- js/script.js
+++ js/script.js
@@ -1,32 +1,64 @@
 
-function parameter(name,value) {
-    // Ex: parameter([nom],[valeur]);
-    let loc = window.location.href, hist= window.history, parameters = loc.match(/[\\?&].([^&#]*)=([^&#]*)/g), data = {}, url = '?';
+// function parameter(name,value) {
+//     let loc = window.location.href, hist= window.history, parameters = loc.match(/[\\?&].([^&#]*)=([^&#]*)/g), data = {}, url = '?';
 
-    for (let key in parameters) {
-        let couple = parameters[key].substring(1, parameters[key].length).split('=');  // A chaque fois qu'on trouve l'occurrence "="
-        data[couple[0]] = couple[1];                                                   // Tableau
-    }
+//     for (let key in parameters) {
+//         let couple = parameters[key].substring(1, parameters[key].length).split('=');  // A chaque fois qu'on trouve l'occurrence "="
+//         data[couple[0]] = couple[1];                                                   // Tableau
+//     }
 
-    if (value == null)  return data[name] ? data[name] : null;
-    if (value != false) data[name] = value;
+//     if (value == null)  return data[name] ? data[name] : null;
+//     if (value != false) data[name] = value;
 
-    for (let key in data) {
-        if (value == false && key == name) continue;                                  // On passe si la valeur est fausse ou si la clé est égale au nom
-        url = url.concat(key.concat('=' + data[key]+'&'));                            // Concaténation de la nouvelle adresse
-    }
-    hist.pushState('', document.title, url.substring(0, url.length-1));               // On modifie l'historique de navigation
-    location.reload();
+//     for (let key in data) {
+//         if (value == false && key == name) continue;                                  // On passe si la valeur est fausse ou si la clé est égale au nom
+//         url = url.concat(key.concat('=' + data[key]+'&'));                            // Concaténation de la nouvelle adresse
+//     }
+//     hist.pushState('', document.title, url.substring(0, url.length-1));               // On modifie l'historique de navigation
+//     location.reload();
+// }
+
+// async function waitingForResponse() {
+//     let param = parameter("q");
+//     if(param != undefined){
+//         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${parameter("key")}&q=${parameter("q")}`);
+//         const todoList = await response.json();
+
+//         recup(todoList);
+//     }
+// }
+// waitingForResponse();
+
+async function waitingForResponse(name) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=bb17b7c52fa045b6aa5113146222906&q=${name}`);
+    const todoList = await response.json();
+    getName(todoList)
+    getTemp(todoList)
+    getCondition(todoList);
+    getWind(todoList);
 }
 
-async function waitingForResponse() {
-    let param = parameter("q");
-    if(param != undefined){
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${parameter("key")}&q=${parameter("q")}`);
-        const todoList = await response.json();
 
-        // console.log(todoList)
-        recup(todoList);
-    }
+// Conditions // 
+function getName(array){
+    // console.log(array.location.name)
+    document.getElementById("city-ttl").innerText = array.location.name;
 }
-waitingForResponse();
+function getTemp(array){
+    // console.log(array.current.temp_c)
+    document.getElementById("condition-tp").innerText = array.current.temp_c + " °C";
+}
+function getCondition(array){
+    // console.log(array.current.condition.text)
+    document.getElementById("condition-c").innerText = array.current.condition.text;
+}
+function getWind(array){
+    // console.log(array.current.wind_kph)
+    document.getElementById("condition-ws").innerText = array.current.wind_kph + " km/h";
+}
+document.getElementById("header-form").addEventListener("submit", function(){
+    waitingForResponse(document.getElementById("input-ville").value);
+    // if (event.target.classList.contains("input-button-ville")) {
+    // }
+});
+
