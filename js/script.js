57ddcf213836d1052a7bdf83fb7f85920e266c80
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-30 11:28:59 +0200

    simple js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-async function waitingForResponse() {
-    const response = await fetch("");
+async function waitingForResponse(name) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=bb17b7c52fa045b6aa5113146222906&q=${name}`);
     const todoList = await response.json();
-    console.table(todoList);
+    getName(todoList)
+    getTemp(todoList)
+    getCondition(todoList);
 }
-waitingForResponse();
+
+
+// Conditions // 
+function getName(array){
+    console.log(array.location.name)
+}
+function getTemp(array){
+    console.log(array.current.temp_c)
+}
+function getCondition(array){
+    console.log(array.current.condition.text)
+}
+
+document.getElementById("header-form").addEventListener("click", function(event){
+    if (event.target.classList.contains("input-button-ville")) {
+        waitingForResponse(document.getElementById("input-ville").value);
+    }
+});
+
+
+
+
