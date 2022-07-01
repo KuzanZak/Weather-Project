64308e5db4b33bc1270db737848d3e5c05bbe7c7
Yann Guichard <xloadx923@msn.com>
2022-07-01 08:52:33 +0200

    close div in excess

--- js/functions.js
+++ js/functions.js
@@ -4,9 +4,9 @@ function recupDay(api,town){
 
     Object.values(api.forecast.forecastday).forEach((daily,index) => {
         const date = 	new Date(daily.date);
-        const days = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche",];
+        const days = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
         contentList.innerHTML += `<dt class="name-town${index}">${days[date.getDay()]} ${daily.date.split('-')[2]}</dt><dd class="icon-weather${index}"><img src="http:${daily.day.condition.icon}" alt=""></dd><dd class="current-temp${index}">${Math.round(daily.day.avgtemp_c)} °C</dd>`;
-        console.log(days[date.getDay()])
+        // console.log(days[date.getDay()])
         
     });
     console.log(api)
