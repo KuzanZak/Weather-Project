c4631d0555b73412b5bc12892ad6b30868cfac58
Yann Guichard <xloadx923@msn.com>
2022-07-07 10:23:22 +0200

    Modif recupDay's function parameters

--- js/functions.js
+++ js/functions.js
@@ -3,11 +3,11 @@ const favoriteJson = JSON.parse(localStorage.getItem("favorites"));
 if(favoriteJson != null && favoriteJson.length > 0) favoriteJson.forEach(favorite => { favoritesTAB.push(favorite) });
 
 // Add week's day on screen
-function recupDay(api,town){
+function recupDay(todoListDay){
     const contentList = document.querySelector('.weatherTown dl');
     contentList.innerHTML = '';
 
-    Object.values(api.forecast.forecastday).forEach((daily,index) => {
+    Object.values(todoListDay.forecast.forecastday).forEach((daily,index) => {
         const date = 	new Date(daily.date);
         const days = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
         contentList.innerHTML += `<dt class="name-town${index}">${days[date.getDay()]} ${daily.date.split('-')[2]}</dt><dd class="icon-weather${index}"><img src="http:${daily.day.condition.icon}" alt=""></dd><dd class="current-temp${index}">${Math.round(daily.day.avgtemp_c)} °C</dd>`;
@@ -15,7 +15,6 @@ function recupDay(api,town){
         // Display forecast when submit
         document.querySelector('.weatherPerday').style.display = 'flex';
     });
-    // console.log(api)
 }
 
 // Favorite button's listener
--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ async function waitingForResponse(name) {
 
     const responseDay = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=bb17b7c52fa045b6aa5113146222906&lang=fr&q=${name}&days=3&aqi=no&alert=no`);
     const todoListDay = await responseDay.json();
-    recupDay(todoListDay,todoList);
+    recupDay(todoListDay);
 }
 
 // Conditions //
